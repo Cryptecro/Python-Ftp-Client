3b9761fe0602ead06c3a3626adaf05e4a4e6c5ad
Zach <thesonyman101@gmail.com>
2016-06-16 21:56:38 -0700

    Update ftpgui.py

--- ftpgui.py
+++ ftpgui.py
@@ -1,33 +1,47 @@
 #Created By:Zachary Sylvester, Python FTP GUI
 from Tkinter import *
 import sys
+from ftplib import FTP
 
 root = Tk()
 root.title("FTP_Client")
 root.geometry('400x400')
-def connect(event):
-	print("Connecting to Server")
-def close(event):
-	print("Bye!!")
+def Ftpad():
+	ftp = ftpt.get
+	user = usert.get
+	pas = past.get
+connected = 0
+if connected == 1:
+	def other():
+		frame = Tk()
+		frame.title("Connected")
+		frame.geometry('400x400')
+		labelCd = Label(frame, text="cd")
+else:
 
-ip = StringVar()
-user = StringVar
-pas = StringVar
+ 	
 
-label_1 = Label(root, text="IP_Address")#Entry text fields for info
-label_2 = Label(root, text="Username")
-label_3 = Label(root, text="Password")
+	ip = StringVar()
+	user = StringVar
+	pas = StringVar
 
-entry_1 = Entry(root,textvariable=ip) 
-entry_2 = Entry(root,textvariable=user)
-entry_3 = Entry(root,textvariable=pas)
+	label_1 = Label(root, text="IP_Address")#Entry text fields for info
+	label_2 = Label(root, text="Username")
+	label_3 = Label(root, text="Password")
 
-label_1.grid(row=0, sticky=E) #IP address text
-label_2.grid(row=1, sticky=E) #User text
-label_3.grid(row=2,	sticky=E) #password text
+	entry_1 = Entry(root,textvariable=ip) 
+	entry_2 = Entry(root,textvariable=user)
+	entry_3 = Entry(root,textvariable=pas)
 
-entry_1.grid(row=0, column=1) #Styleing for entry
-entry_2.grid(row=1, column=1) 
-entry_3.grid(row=2, column=1) 
+	label_1.grid(row=0, sticky=E) #IP address text
+	label_2.grid(row=1, sticky=E) #User text
+	label_3.grid(row=2,	sticky=E) #password text
 
-root.mainloop()
+	entry_1.grid(row=0, column=1) #Styleing for entry
+	entry_2.grid(row=1, column=1) 
+	entry_3.grid(row=2, column=1)
+
+	button1 = Button(root,text="Submit")
+
+
+	root.mainloop()
